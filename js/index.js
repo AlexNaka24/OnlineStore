0a607445829da3e46e74e6eeaf2ea3cb186653d8
Alex Nakagama <alexnakagama4@gmail.com>
2025-09-09 20:48:44 -0300

    ...

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+const inicio = document.querySelector('.nav-inicio');
+const servicios = document.querySelector('.nav-servicios');
+const productos = document.querySelector('.nav-productos');
+const cursos = document.querySelector('.nav-cursos');
+const contacto = document.querySelector('.nav-contacto');
+
+inicio.addEventListener('click', () => {
+    // logica para manejar el clic en "Inicio"
+});
+
+servicios.addEventListener('click', () => {
+    // logica para manejar el clic en "Servicios"
+});
+
+productos.addEventListener('click', () => {
+    // logica para manejar el clic en "Productos"
+});
+
+cursos.addEventListener('click', () => {
+    // logica para manejar el clic en "Cursos"
+});
+
+contacto.addEventListener('click', () => {
+    // logica para manejar el clic en "Contacto"
+});
