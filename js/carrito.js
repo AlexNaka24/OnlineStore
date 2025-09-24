d8f76653a1965e838d61a73be9cb48ddf3ee66b1
Alex Nakagama <alexnakagama4@gmail.com>
2025-09-23 21:10:41 -0300

    hacer media query

--- js/carrito.js
+++
@@ -1,3 +0,0 @@
-import { mostrarModalContacto } from './modules/modal-contacto.js';
-
-mostrarModalContacto();
--- js/productos.js
+++
@@ -1,5 +0,0 @@
-import { mostrarModalContacto } from "./modules/modal-contacto.js";
-import { moverFiltro } from "./modules/move-filter.js";
-
-mostrarModalContacto();
-moverFiltro();
