c607c7177d9072d99fd73abcb508757d6b15f7ae
Alex Nakagama <alexnakagama4@gmail.com>
2025-11-18 14:21:29 -0300

    btn me lleve a contacto

---
+++ js/pages/servicios.js
@@ -0,0 +1,10 @@
+
+const btnReserva = document.querySelector('.btn-reserva');
+
+if (btnReserva) {
+    btnReserva.addEventListener('click', function () {
+
+        window.location.href = 'contacto.html';
+    })
+}
+
